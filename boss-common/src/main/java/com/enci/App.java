33428fdcda9819c6e48804b66033095e13513f88
minhuayong <minhuayong@aliyun.com>
2017-03-02 11:29:42 +0800

    初始化项目

---
+++ boss-common/src/main/java/com/enci/App.java
@@ -0,0 +1,13 @@
+package com.enci;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
