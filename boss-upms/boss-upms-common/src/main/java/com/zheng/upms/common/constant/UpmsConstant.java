005aa7cc532e9a90f51c485672f5ef9f855cfb45
minhuayong <minhuayong@aliyun.com>
2017-03-02 11:33:52 +0800

    初始化项目

---
+++ boss-upms/boss-upms-common/src/main/java/com/zheng/upms/common/constant/UpmsConstant.java
@@ -0,0 +1,11 @@
+package com.zheng.upms.common.constant;
+
+import com.zheng.common.base.BaseConstants;
+
+/**
+ * upms系统常量类
+ * Created by shuzheng on 2017/2/18.
+ */
+public class UpmsConstant extends BaseConstants {
+
+}
