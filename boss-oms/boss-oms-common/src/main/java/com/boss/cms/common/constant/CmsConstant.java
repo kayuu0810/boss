83092f6e96c16331d6e846e9101b54eced092a03
minhuayong <minhuayong@aliyun.com>
2017-03-02 17:49:17 +0800

    加入清除缓存功能

---
+++ boss-oms/boss-oms-common/src/main/java/com/boss/cms/common/constant/CmsConstant.java
@@ -0,0 +1,11 @@
+package com.boss.cms.common.constant;
+
+import com.boss.common.base.BaseConstants;
+
+/**
+ * cms系统常量类
+ * Created by shuzheng on 2017/2/19.
+ */
+public class CmsConstant extends BaseConstants {
+
+}
---
+++ boss-oms/boss-oms-dao/src/main/java/com/boss/cms/dao/Generator.java
@@ -0,0 +1,27 @@
+package com.boss.cms.dao;
+
+import com.boss.common.util.MybatisGeneratorUtil;
+import com.boss.common.util.PropertiesFileUtil;
+
+/**
+ * 代码生成类
+ * Created by ZhangShuzheng on 2017/1/10.
+ */
+public class Generator {
+
+	// 根据命名规范，只修改此常量值即可
+	private static String MODULE_PREFIX_NAME = "oms";
+	private static String JDBC_DRIVER = PropertiesFileUtil.getInstance("generator").get("generator.jdbc.driver");
+	private static String JDBC_URL = PropertiesFileUtil.getInstance("generator").get("generator.jdbc.url");
+	private static String JDBC_USERNAME = PropertiesFileUtil.getInstance("generator").get("generator.jdbc.username");
+	private static String JDBC_PASSWORD = PropertiesFileUtil.getInstance("generator").get("generator.jdbc.password");
+
+	/**
+	 * 自动代码生成
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MybatisGeneratorUtil.generator(JDBC_DRIVER, JDBC_URL, JDBC_USERNAME, JDBC_PASSWORD, MODULE_PREFIX_NAME);
+	}
+
+}
